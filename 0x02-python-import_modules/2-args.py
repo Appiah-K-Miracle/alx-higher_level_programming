77af294fa475a9747e3f8cd04836514069af4696
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-04 05:10:23 -0800

    argument count letter

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    letter_lenght = len(argv) - 1
+    if letter_lenght < 1:
+        print("{} arguments ".format(letter_lenght))
+    elif letter_lenght == 1:
+        print("{} aryument:".format(letter_lenght))
+    else:
+        print("{} arguments:".format(letter_lenght))
+
+    for count in range(letter_lenght):
+        print("{}: {:s}".format(count + 1, argv[count + 1]))
