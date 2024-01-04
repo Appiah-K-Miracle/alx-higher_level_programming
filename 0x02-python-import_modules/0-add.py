312046d097a97c90f4ed8f000d98928f6bc2b81d
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-04 04:11:18 -0800

    add two int number

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    first_number = 1
+    second_number = 2
+    print("{} + {} = {}".format(first_number, second_number, add(first_number, second_number)))
