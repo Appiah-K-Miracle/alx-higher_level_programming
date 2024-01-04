ba783ac01212a1107ad5eac8115bdd7e36dbd8fa
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-04 05:53:42 -0800

    show all function without underscore

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    entire_names = dir()
+    for hide in range(0, len(entire_names)):
+        if entire_names[hide][:2] !="_":
+            print("{:s}".format(entire_names[hide]))
