7ddfa52909dc9f18fab251e12885226fb23f87c8
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-03 19:20:47 -0800

    add more condition

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastNumber = abs(number)%10
+if number number < 0:
+    lastNumber = -(lastNumber)
+theWord = "Last digit of ", number ,"is ",lastNumber
+if lastNumber > 5:
+    print(theWord," and is greater than 5")
+elif lastNumber == 0:
+    print(theWord," and is 0")
+elif lastNumber < 6:
+    print(theWord, " and is less than 6 and nnot 0")
