dfbf754e2e50975e8e04fbbbe08057dc9dccca00
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-07 23:49:10 -0800

    remove letter c(upper and lower case) from the string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string[:]:
-        create_new_string = my_string.translate({ord("c") : None})
-        create_new_second_string = my_string.translate({ord("C") : None})
+        create_new_string = my_string.translate({ord("c"): None})
+        create_new_second_string = my_string.translate({ord("C"): None})
         return create_new_second_string
     return my_string
