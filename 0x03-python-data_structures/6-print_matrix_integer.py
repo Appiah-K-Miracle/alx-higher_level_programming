b95bcd73d5217363e7cf25ffbd225906c52cbef0
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-08 01:21:02 -0800

    hello matrix'

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for i in matrix:
-            for j in i:
-                print("{:d}".format(j), end= ' ' if j != i[-1] else '')
+        for index in matrix:
+            for items in index:
+                print("{:d}".format(items), end=' ' if items != index[-1] else '')
             print()
