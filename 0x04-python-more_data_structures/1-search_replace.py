d6770806c006b3b47407d086fa857d2cfde4b60f
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-10 20:42:53 -0800

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_new_list = list(map(lambda line: replace if line == search else line, my_list))
-    return my_new_list
+    new_list = list(map(lambda line: replace if line == search else line, my_list))
+    return new_list
