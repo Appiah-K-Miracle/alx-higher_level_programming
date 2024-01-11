dd0468f5a01c804ae7ee057d88a32b8a2295c0ac
Appiah-K-Miracle <amdwatkins123gmail.com>
2024-01-10 17:07:30 -0800

    square of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_value = [ ]
+    square_value = []
     for index in matrix:
         square_value.append([num**2 for num in index])
     return square_value
